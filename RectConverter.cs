3a953254e24bee512bd239e38cdc963fcf98442e
co44ee <anastasia.ovtsunova@gmail.com>
2025-04-11 19:33:13 +0300

    fix 2.0

---
+++ RectConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace testapp
+{
+    public class RectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 &&
+                values[0] is double width &&
+                values[1] is double height &&
+                !double.IsNaN(width) &&
+                !double.IsNaN(height))
+            {
+                return new Rect(0, 0, width, height);
+            }
+
+            return Rect.Empty;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
